3a5f6218689bf5521bb39ee7e3c637f7a23f8499
clwater <gzb@clwater.top>
2022-03-18 11:34:35 +0800

    WIP(oss): 认证及获取列表

--- app/src/main/java/com/clwater/oss_android/MainActivity.kt
+++ app/src/main/java/com/clwater/oss_android/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.clwater.oss_android
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,39 +17,49 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.clwater.oss_android.manager.ALiOssManager
 import com.clwater.oss_android.manager.FuelManager
 import com.clwater.oss_android.manager.FuelManager.STSModelCallBack
 import com.clwater.oss_android.ui.theme.Oss_AndroidTheme
 import com.google.gson.Gson
+import java.util.logging.Logger
 
 class MainActivity : ComponentActivity() {
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
 
+        context = this
         initData()
-
-
 
 //        ALiOssManager.
     }
 
     private fun initData() {
-        val callback: STSModelCallBack = object : STSModelCallBack {
-            override fun fail() {}
-            override fun call(stsModel: STSModel) {
-                Log.d("gzb", "STSModel: ${Gson().toJson(stsModel)}")
-                genALiOssInfo(stsModel)
-            }
-        }
-        FuelManager.getToken(callback)
+        ALiOssManager.init(this)
+        ALiOssManager.getAllObject()
     }
 
-    private fun genALiOssInfo(stsModel: STSModel){
-        Constants.OSS_ACCESS_KEY_ID = stsModel.AccessKeyId
-        Constants.OSS_ACCESS_KEY_SECRET = stsModel.AccessKeySecret
-        Constants.OSS_STS_TOKEN = stsModel.SecurityToken
-    }
+//    private fun initData() {
+//        val callback: STSModelCallBack = object : STSModelCallBack {
+//            override fun fail() {}
+//            override fun call(stsModel: STSModel) {
+//                Log.d("gzb", "STSModel: ${Gson().toJson(stsModel)}")
+//                genALiOssInfo(stsModel)
+//            }
+//        }
+//        FuelManager.getToken(callback)
+//    }
+
+//    private fun genALiOssInfo(stsModel: STSModel){
+//        Constants.OSS_ACCESS_KEY_ID = stsModel.AccessKeyId
+//        Constants.OSS_ACCESS_KEY_SECRET = stsModel.AccessKeySecret
+//        Constants.OSS_STS_TOKEN = stsModel.SecurityToken
+//
+//        ALiOssManager.init(this)
+//        ALiOssManager.getAllObject()
+//    }
 
 
     private fun initView() {
@@ -71,31 +76,42 @@ class MainActivity : ComponentActivity() {
         })
 
     }
-}
 
-@Composable
-fun Greeting(title: String) {
-    Column() {
-        TopAppBar(title = { Text(text = title) })
+    @Composable
+    fun Greeting(title: String) {
+        Column() {
+            TopAppBar(title = { Text(text = title) })
 
-        Column(Modifier.padding(12.dp, 12.dp)) {
-            Image(
-                painter = painterResource(id = R.mipmap.test),
-                contentDescription = "contentDescription",
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-            )
-            Text(text = "Hello")
+            Column(Modifier.padding(12.dp, 12.dp)) {
+                Image(
+                    painter = painterResource(id = R.mipmap.test),
+                    contentDescription = "contentDescription",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                )
+                Text(text = "Hello")
+
+                Button(
+                    onClick = {
+                        Log.d("gzb", "click")
+//                    ALiOssManager.init(context)
+//                    ALiOssManager.getAllObject()
+                        initData()
+                    },
+                    contentPadding = PaddingValues(
+                        start = 20.dp,
+                        top = 12.dp,
+                        end = 20.dp,
+                        bottom = 12.dp
+                    )
+                ) {
+                    Text("Test")
+                }
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Oss_AndroidTheme {
-        Greeting(stringResource(id = R.string.app_name))
-    }
-}
+
--- app/src/main/java/com/clwater/oss_android/manager/ALiOssManager.kt
+++ app/src/main/java/com/clwater/oss_android/manager/ALiOssManager.kt
@@ -7,22 +7,34 @@ import com.alibaba.sdk.android.oss.OSS
 import com.alibaba.sdk.android.oss.OSSClient
 import com.alibaba.sdk.android.oss.ServiceException
 import com.alibaba.sdk.android.oss.callback.OSSCompletedCallback
+import com.alibaba.sdk.android.oss.common.auth.OSSAuthCredentialsProvider
+import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider
+import com.alibaba.sdk.android.oss.common.auth.OSSStsTokenCredentialProvider
 import com.alibaba.sdk.android.oss.internal.OSSAsyncTask
 import com.alibaba.sdk.android.oss.model.ListObjectsRequest
 import com.alibaba.sdk.android.oss.model.ListObjectsResult
+import com.clwater.oss_android.Constants
+import com.google.gson.Gson
 
 
 /**
  * @author: gengzhibo
  * @date: 2022/3/17
  */
-class ALiOssManager {
+object ALiOssManager {
     private var marker: String? = null
     private var isCompleted = false
 
-    lateinit var oss: OSS
+    private lateinit var oss: OSS
     fun init(context: Context){
-//        oss = OSSClient(context, Constants.endpoint, null)
+        val credentialProvider = OSSAuthCredentialsProvider(Constants.STS_SERVER_URL)
+//        val credentialProvider: OSSCredentialProvider = OSSStsTokenCredentialProvider(
+//            Constants.OSS_ACCESS_KEY_ID,
+//            Constants.OSS_ACCESS_KEY_SECRET,
+//            Constants.OSS_STS_TOKEN
+//        )
+
+        oss = OSSClient(context, Constants.endpoint, credentialProvider)
     }
 
     // 分页列举所有object
@@ -37,8 +49,7 @@ class ALiOssManager {
 
     // 列举一页文件。
     fun getObjectList(): OSSAsyncTask<*> {
-        val request = ListObjectsRequest("examplebucket")
-        // 填写每页返回文件的最大个数。如果不设置此参数，则默认值为100，maxkeys的取值不能大于1000。
+        val request = ListObjectsRequest(Constants.BUCKET_NAME)
         // 填写每页返回文件的最大个数。如果不设置此参数，则默认值为100，maxkeys的取值不能大于1000。
         request.maxKeys = 20
         request.marker = marker
@@ -47,7 +58,7 @@ class ALiOssManager {
             object : OSSCompletedCallback<ListObjectsRequest?, ListObjectsResult> {
                 override fun onSuccess(request: ListObjectsRequest?, result: ListObjectsResult) {
                     for (objectSummary in result.objectSummaries) {
-                        Log.i("ListObjects", objectSummary.key)
+                        Log.i("ListObjects", Gson().toJson(objectSummary))
                     }
                     // 最后一页。
                     if (!result.isTruncated) {
