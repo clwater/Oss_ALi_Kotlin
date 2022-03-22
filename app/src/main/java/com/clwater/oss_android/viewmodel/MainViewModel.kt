e884f8e55efc198112fb895f0e9d2d83de37713e
clwater <gzb@clwater.top>
2022-03-22 11:43:55 +0800

    WIP(viewModel): 观察展示

--- app/src/main/java/com/clwater/oss_android/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/clwater/oss_android/viewmodel/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.clwater.oss_android.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.alibaba.sdk.android.oss.ClientException
@@ -10,73 +8,27 @@ import com.alibaba.sdk.android.oss.model.ListObjectsRequest
 import com.alibaba.sdk.android.oss.model.ListObjectsResult
 import com.clwater.oss_android.manager.ALiOssManager
 import com.clwater.oss_android.model.OssFileModel
-import com.clwater.oss_android.model.STSModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class MainViewModel : ViewModel() {
-//    var stsModel: MutableLiveData<List<OssFileModel>>
-    var test: MutableLiveData<Int>
-
-    init{
-//        stsModel = MutableLiveData<List<OssFileModel>>()
-        test = MutableLiveData<Int>()
-    }
-
-//    private val _stsModel: MutableLiveData<List<OssFileModel>> = MutableLiveData<List<OssFileModel>>()
-
-
-    /** Add hard code test data**/
-//    init {
-//        _stsModel.value = getSTSList()
-//    }
-
-//    fun onDevicesChange(listDevices: List<OssFileModel>) {
-//        _stsModel.value = listDevices
-//    }
+    var stsModel: MutableLiveData<List<OssFileModel>> = MutableLiveData<List<OssFileModel>>()
+    var errorCode: MutableLiveData<String> = MutableLiveData()
 
     fun getSTSInfo(){
-//        test.value = 6
-//    Log.d("gzb", "getSTSInfo")
         val callback = object : ALiOssManager.ALiOssCallBack {
             override fun onResult(request: ListObjectsRequest?, result: ListObjectsResult) {
-//                Log.d("gzb", "onResult")
-//                Log.d("gzb", "" + Gson().toJson(result));
-
-//                updateListInfo(Gson().toJson(result.objectSummaries), true)
-
-//                Log.d("gzb", "onResult1")
-                test.value = 66
-
-                result.objectSummaries.forEach {
-                    test.value = 6666
-
-                    val ossFileModel = OssFileModel(it.key, it.lastModified.toString(), it.size.toString())
-                    val itemType = object : TypeToken<List<OssFileModel>>() {}.type
-//                    stsModel.value =  Gson().fromJson(Gson().toJson(listOf(ossFileModel)), itemType)
-                    test.value = 66666
-
-                }
-                test.value = 666
-
-//                val itemType = object : TypeToken<List<OssFileModel>>() {}.type
-//                stsModel =  Gson().fromJson(Gson().toJson(result.objectSummaries), itemType)
-//                Log.d("gzb", "onResult2")
-
-//                Log.d("gzb", "" + Gson().toJson(stsModel));
+                val itemType = object : TypeToken<List<OssFileModel>>() {}.type
+                stsModel.postValue(Gson().fromJson( Gson().toJson(result.objectSummaries), itemType))
             }
             override fun onFail(
                 request: ListObjectsRequest?,
                 clientException: ClientException,
                 serviceException: ServiceException
             ) {
-                test.value = 777
-
-                Log.d("gzb", "fail")
-
+                errorCode.postValue("fail")
             }
         }
-
 
         ALiOssManager.getObjectList(callback)
     }
