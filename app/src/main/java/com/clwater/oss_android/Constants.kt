b9b979af5fbf32b4686df84ce943f96f03d387a4
clwater <gzb@clwater.top>
2022-03-28 09:51:04 +0800

    WIP(build): 配置文件更新

---
+++ app/src/main/java/com/clwater/oss_android/Constants.kt
@@ -0,0 +1,18 @@
+package com.clwater.oss_android
+
+/**
+ * @author: gengzhibo
+ * @date: 2022/3/17
+ */
+class Constants {
+    companion object {
+        var endpoint = "http://oss-cn-beijing.aliyuncs.com"
+        val BUCKET_NAME = ""
+        var OSS_ACCESS_KEY_ID = ""
+        var OSS_ACCESS_KEY_SECRET = ""
+        var OSS_STS_TOKEN = ""
+
+        //sts服务器地址
+        val STS_SERVER_URL = ""
+    }
+}
